0828b8b1b54c5d821c6bd7d8ceb43e87782f7cab
Sayjel Patel <sayjel.v.patel@gmail.com>
2020-04-19 12:57:31 +0400

    updates

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,22 +1,55 @@
-console.log('hi')
+console.log('sketch.js')
+let spores = []
+
 function setup() {
 
-	// var myDiv = document.getElementByID('sketch-div');
-	// console.log('myDiv',myDiv.style)
-	// var myWidth = myDiv.style.width;
-
-
-    var canvas = createCanvas(windowWidth/8, windowWidth/8,WEBGL);
+    var canvas = createCanvas(windowWidth / 8, windowWidth / 8, WEBGL);
     canvas.parent('sketch-div');
     stroke(0)
     noFill()
+    for (i = 0; i < TWO_PI; i+=PI/6) {
+    	for (j=0; j<TWO_PI; j+=PI/4){
+    		for (k=0; k<TWO_PI;k+=PI/4){
+    			spores.push(new Spore(i,j,k))
+    		}
+    	}
+        
 
+    }
 }
 
 function draw() {
     background('#F04E30')
-    rotateX(frameCount*0.001+mouseX*0.001)
-    rotateZ(frameCount*0.001+mouseY*0.001)
-    sphere(width/4)
+    scale(Math.abs(sin(frameCount*0.001))*0.5+0.75)
+    rotateX(frameCount * 0.005 + mouseX * 0.001)
+    rotateZ(frameCount * 0.005 + mouseY * 0.001)
+    let radius = width / 4
+    sphere(radius,8,8)
+
+    spores.forEach(s => s.display(radius))
+
+}
+
+function Spore(i,j,j) {
+
+    this.rx = i 
+    this.ry = j
+    this.rz = k
+    this.display = function(radius) {
+        push()
+        rotateZ(this.rz)
+        rotateX(this.rx)
+        rotateY(this.ry)
+        
+        translate(0, -radius, 0)
+        cone(radius / 16, radius/2,4);
+        push()
+        translate(0, -radius*0.3, 0)
+        sphere(radius / 16,4,4)
+        pop()
+        pop()
+
+
+    }
 
 }
