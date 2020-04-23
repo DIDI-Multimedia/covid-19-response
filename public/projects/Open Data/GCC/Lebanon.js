fe20a64fa9c1207b9b9375fc8a77fd2fac1e2e85
mona <mona.ityy@gmail.com>
2020-04-23 13:33:40 +0300

    ignore pls

--- public/projects/Open Data/GCC/Lebanon.js
+++ public/projects/Open Data/GCC/Lebanon.js
@@ -1,20 +1,28 @@
-INFECTED: 
-
-DEATHS:
-Age: 
-Gender : 
-Nationality: 
-
-
-Nationality: 
-
+INFECTED: 7 cases
 
 By Date:
 
 MAR 
-	
+	20 = 2 D
+	29 = 1 D
+	30 = 2 D
+	31 = 1 D
 APR
-	
+	1 = 1 D
+	3 = 1 D
+	4 = 1 D
+	5 = 1 D
+	6 = 1 D
+	7 = 1 D
+	9 = 2 D
+	10 = 2 D
+	11 = 4 D
+	12 - 2 D
+	13 - 3 D
+	14 - 3 D
+	15 - 5 D
+	16 - 2 D
+	17 - 2 D 
 MAY
 JUN
 JUL
