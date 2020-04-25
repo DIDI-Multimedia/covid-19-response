d4e4ea0ac7d768cb53d1fc7873893de8db641f2f
Sayjel Patel <sayjel.v.patel@gmail.com>
2020-04-25 18:48:02 +0400

    updates

--- public/script.js
+++ public/script.js
@@ -4,3 +4,20 @@ console.log('load JQUERY')
 $("concept").click(function(){
   $("p").hide();
 });
+
+
+var fullscreen = document.getElementById('play-fullscreen'),
+player = document.getElementById('player');
+
+fullscreen.addEventListener('click', function (e) {
+    if (~player.src.indexOf('?')) player.src += '&autoplay=1';
+    else player.src += '?autoplay=1';
+
+    var req = player.requestFullscreen
+        || player.webkitRequestFullscreen
+        || player.mozRequestFullScreen
+        || player.msRequestFullscreen;
+
+    req.call(player);
+    e.preventDefault();
+});
